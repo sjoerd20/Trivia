20342794f1dc4698d63275ad4d821789bb06e427
sjoerd20 <sjoerd.terpstra@outlook.com>
2018-12-08 17:43:28 +0100

    Added code to TriviaRequest

--- app/src/main/java/com/example/sjoerd/trivia/GamePlayActivity.java
+++ app/src/main/java/com/example/sjoerd/trivia/GamePlayActivity.java
@@ -2,12 +2,29 @@ package com.example.sjoerd.trivia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class GamePlayActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class GamePlayActivity extends AppCompatActivity implements TriviaRequest.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
+
+        // request questions
+        TriviaRequest triviaRequest = new TriviaRequest(this);
+        triviaRequest.getQuestions(this);
+    }
+
+    @Override
+    public void gotQuestions(ArrayList<Question> questions) {
+        Toast.makeText(this, "Questions received!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void gotQuestionsError(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
